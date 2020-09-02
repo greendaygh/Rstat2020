0f14936959e8c89e1e85d5180a4525af6c60aedc
haseong <kimhaseong@gmail.com>
2020-09-03 01:21:01 +0900

    update

---
+++ 01-code.R
@@ -0,0 +1,27 @@
+x <- 1
+y <- 2
+z <- 3
+
+
+# x <- 20
+# y <- 30
+
+x = 1
+
+bar
+
+mylog <- function(x) {
+  y <- log(x)
+  return(y)
+}
+
+
+x <- c(10, 20, 30)
+log(x)
+
+library(UsingR)
+help(package="UsingR")
+
+data(babies)
+babies
+data(package="UsingR")
